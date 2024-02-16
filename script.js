c5ca376eded5615158982d250ac4786ae1dd87d5
Ivan Quintana <ivanquintana3010.iq@gmail.com>
2024-02-16 11:57:07 -0700

    4th-mod

---
+++ script.js
@@ -0,0 +1,17 @@
+var encryptButton = document.querySelector(".encrypt");
+var decryptButton = document.querySelector(".decrypt");
+
+
+
+
+encryptButton.onclick = encrypt;
+decryptButton.onclick = decrypt;
+
+
+function encrypt(){
+    let text = document.getElementById("content-textarea").value; 
+
+    let encryptedText = text
+
+    
+}
