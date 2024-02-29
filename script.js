c2ffe04a9befff0eb10e91c6dca0a2c44eef88e8
Ivan Quintana <ivanquintana3010.iq@gmail.com>
2024-02-28 18:22:50 -0700

    2/28 update
    added the crypt function based on the instructional video

---
+++ script.js
@@ -0,0 +1,54 @@
+//Variables
+
+const textArea = document.querySelector(".content-textarea");
+const message = document.querySelector(".crypted-message");
+
+//Crypt Button
+
+function btnCrypt(){
+    const cryptedText = crypt(textArea.value);
+    message.value = cryptedText
+    textArea.value =""
+}
+
+function crypt(cryptedString){
+    let matrixCode = [["e","enter"], ["i", "imes"], ["a","ai"], ["o", "ober"], ["u", "ufat"]];
+    cryptedString = cryptedString.toLowerCase()
+
+    for(let i = 0; i < matrixCode.length; i++){
+        if(cryptedString.includes(matrixCode[i][0])){
+            cryptedString = cryptedString.replaceAll(matrixCode[i][0], matrixCode[i][1])
+
+        }       
+    }
+    return cryptedString
+}
+
+//Decrypt Button
+
+function btnDecrypt(){
+    const cryptedText = decrypt(textArea.value);
+    message.value = cryptedText
+    textArea.value =""
+}
+
+function decrypt(decryptedString){
+    let matrixCode = [["e","enter"], ["i", "imes"], ["a","ai"], ["o", "ober"], ["u", "ufat"]];
+    decryptedString = decryptedString.toLowerCase()
+
+    for(let i = 0; i < matrixCode.length; i++){
+        if(decryptedString.includes(matrixCode[i][1])){
+            decryptedString = decryptedString.replaceAll(matrixCode[i][1], matrixCode[i][0])
+
+        }       
+    }
+    return decryptedString
+}
+
+
+
+
+
+
+
+
